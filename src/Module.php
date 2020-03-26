c25ceb9fdbe4f6867ed4f54a7c24f66d1bdd8f6b
Oleh Boiko <mackraiscms@gmail.com>
2020-03-26 09:53:31 +0200

    change namespace

---
+++ src/Module.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * @user: MackRais
+ * @site: http://mackrais.com
+ * @email: mackraiscms@gmail.com
+ */
+namespace  mackrais\portfolio;
+
+use yii\base\BootstrapInterface;
+
+class Module extends \yii\base\Module implements BootstrapInterface
+{
+
+    public function bootstrap($app)
+    {
+//        if ($app instanceof \yii\console\Application) {
+//            $this->controllerNamespace = 'app\modules\sitemap\commands';
+//        }
+    }
+}
+
+
